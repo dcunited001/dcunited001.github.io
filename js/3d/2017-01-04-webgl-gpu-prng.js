ef7ada52a54ad097197196af4d59607835c0af29
David Conner <dconner.pro@gmail.com>
2017-01-06 04:21:23 -0500

    added pause button to the random cube

--- js/3d/2017-01-04-webgl-gpu-prng.js
+++ js/3d/2017-01-04-webgl-gpu-prng.js
@@ -2,12 +2,13 @@
 
 var container;
 var cam, origCamZ;
-var scene, renderer, texLoader;
+var scene, renderer, paused = false;
 var cube, cubeSize, cubeGeo, cubeTexture, cubeMaterial;
 var cubeRotationAxis = new THREE.Vector3(0.3,0.4,0.5), cubeRotationRate = Math.PI / 5;
-var texRng, gpuCompute, computeTexture, computeMaterial, randomVariable, randomUniforms;
+var texRng, gpuCompute, randomVariable, randomUniforms;
 var startTime = new Date().getTime(), currentTime = startTime, elapsedTime = startTime - currentTime;
 
+cubeSize = 500;
 var WIDTH = 64, HEIGHT = 64;
 var mouseX = 0, mouseY = 0;
 var windowHalfX = window.innerWidth / 2;
@@ -21,12 +22,9 @@ function init() {
   cam.position.z = origCamZ;
 
   scene = new THREE.Scene();
-
-  texLoader = new THREE.TextureLoader();
 }
 
 function createCube() {
-  var cubeSize = 500;
   cubeGeo = new THREE.BoxBufferGeometry(cubeSize, cubeSize, cubeSize, 10, 10, 10);
 
   cubeMaterial = new THREE.MeshBasicMaterial({
@@ -44,18 +42,9 @@ function createCube() {
   //  fragmentShader: document.getElementById('fragCube').textContent
   //});
 
-  console.log(cubeMaterial);
-
   cube = new THREE.Mesh(cubeGeo, cubeMaterial);
   cube.position.set(0,0,1);
   scene.add(cube);
-
-  console.log(cubeTexture);
-
-  //cubeGroup = THREE.SceneUtils.createMultiMaterialObject(cube, [computeMaterial]);
-  //cubeGroup = THREE.SceneUtils.createMultiMaterialObject(cube, [computeMaterial, computeMaterial2]);
-  //cubeGroup.position.set(0,0,1);
-  //scene.add(cubeGroup);
 }
 
 function createGPUCompute() {
@@ -118,6 +107,11 @@ function onDocMouseMove() {
   mouseY = (event.clientY - windowHalfY);
 }
 
+function togglePause() {
+  paused = !paused;
+  document.getElementById('btn-pause').textContent = (paused ? "Play" : "Pause");
+}
+
 function animate() {
   requestAnimationFrame(animate);
   update();
@@ -142,40 +136,24 @@ function render() {
   cam.position.z = origCamZ + dist;
   cam.lookAt(scene.position);
 
-  //cubeMaterial.uniforms.texture.value = gpuCompute.getCurrentRenderTarget(randomVariable).texture;
-  cubeMaterial.map = gpuCompute.getCurrentRenderTarget(randomVariable).texture;
-  gpuCompute.compute();
-  cubeMaterial.needsUpdate = true;
+  if (!paused) {
+    //cubeMaterial.uniforms.texture.value = gpuCompute.getCurrentRenderTarget(randomVariable).texture;
+    cubeMaterial.map = gpuCompute.getCurrentRenderTarget(randomVariable).texture;
+    gpuCompute.compute();
+    cubeMaterial.needsUpdate = true;
 
-  //texRng = gpuCompute.getCurrentRenderTarget(randomVariable).texture;
-  //gpuCompute.renderTexture(texRng, randomVariable.renderTargets[0]);
-  //cubeMaterial.needsUpdate = true;
-  //texRng.image.data = gpuCompute.getCurrentRenderTarget(randomVariable).texture;
-
+    //texRng = gpuCompute.getCurrentRenderTarget(randomVariable).texture;
+    //gpuCompute.renderTexture(texRng, randomVariable.renderTargets[0]);
+    //cubeMaterial.needsUpdate = true;
+    //texRng.image.data = gpuCompute.getCurrentRenderTarget(randomVariable).texture;
+  }
+  
   renderer.render(scene, cam);
 }
 
 init();
 createRenderer();
 createGPUCompute();
-
 createCube();
 configureCanvas();
-
-console.log("before compute");
-console.log(texRng);
-
-gpuCompute.compute();
-cubeMaterial.needsUpdate = true;
-var tempTexture = gpuCompute.getCurrentRenderTarget(randomVariable).texture;
-console.log("before render");
-console.log(tempTexture);
-//texRng = gpuCompute.getCurrentRenderTarget(randomVariable).texture;
-
-cubeMaterial.needsUpdate = true;
-console.log("after render");
-//console.log(tempTexture);
-console.log(texRng);
-
-//computeMaterial.needsUpdate = true;
 animate();
