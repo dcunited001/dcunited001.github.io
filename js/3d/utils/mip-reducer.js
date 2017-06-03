6f738c46362b356e702f1181a07922ba2ff37408
David Conner <dconner.pro@gmail.com>
2017-06-03 08:09:03 -0400

    rewrite mip reducer to avoid using generateMipmaps()

--- js/3d/utils/mip-reducer.js
+++ js/3d/utils/mip-reducer.js
@@ -20,51 +20,52 @@
 
 // TODO: refactor attachments to instances of a class
 class MipReducerAttachment {
-  constructor() {
+  constructor(attr) {
+    this._id = attr.id;
+    this._defaultValue = attr.defaultValue;
+    this._internalFormat = attr.internalFormat;
+    this._init = attr.init;
+    this._calc = attr.calc;
+    this._write = attr.write;
 
+    this._textures = [];
+  }
+
+  get id() { return this._id; }
+  get defaultValue() { return this._defaultValue; }
+  get internalFormat() { return this._internalFormat; }
+  get init() { return this._init; }
+  get calc() { return this._calc; }
+  get write() { return this._write; }
+
+  get textures() { return this._textures; }
+  set textures(textures) { this._textures = textures; }
+
+  addTexture(texture) {
+    this._textures.push(texture);
   }
 }
 
 class MipReducer {
-
   constructor(resolution, attachments, options = {}) {
     this._resolution = resolution;
-    this._requiredUniforms = ['u_mipmapLevel'];
-    this._attachments = attachments;
+    this._requiredUniforms = ['u_iteration', 'u_readOffset', 'u_readSize', 'u_writeOffset', 'u_writeSize'];
 
     var logX = Math.log2(this._resolution[0]),
       logY = Math.log2(this._resolution[1]);
 
     this._levels = (logX > logY ? Math.trunc(logX) + 1 : Math.trunc(logY) + 1);
-    this._mipmaps = new Int32Array(this._levels - 1).reduce((mipmaps, m, i) => {
-      var lastMipmap = mipmaps[i];
+    this._mipmaps = this.generateMipmaps(this._resolution, this._levels);
+    this._attachments = attachments;
+    this._values = {};
+    this._pingpongId = 0;
 
-      var width = Math.floor(lastMipmap.x / 2),
-        height = Math.floor(lastMipmap.y / 2);
-
-      mipmaps[i+1] = {
-        x: (width > 0) ? width : 1,
-        y: (height > 0) ? height : 1
-      };
-      return mipmaps;
-    }, [{x: resolution[0], y: resolution[1]}]);
-
-    console.log(this._mipmaps);
-
-    this._debug = options.debug;
-
-    // TODO: after createTextures(), then:
-    // - bind framebuffer
-    // - clear
-    // - finally generate mipmaps
+    this._uniforms = options.uniforms || {};
   }
 
   configure(context, options = {}) {
-    this._textures = this.createTextures(context, this._resolution);
-
-    this._framebuffer = this.createFramebuffer(context);
-    //this.createMipmaps(context);
-
+    this.createTextures(context);
+    this.createFramebuffers(context);
     this.configureProgram(context);
     this.configureUniforms(context, options);
 
@@ -73,52 +74,74 @@ class MipReducer {
     context.samplerParameteri(this._samplerNearest, context.TEXTURE_MAG_FILTER, context.NEAREST);
     context.samplerParameteri(this._samplerNearest, context.TEXTURE_WRAP_S, context.CLAMP_TO_EDGE);
     context.samplerParameteri(this._samplerNearest, context.TEXTURE_WRAP_T, context.CLAMP_TO_EDGE);
-
-    if (this._debug) {
-      this._debugTextures = this.createTexutres(context, this._resolution);
-    }
   }
 
-  createTextures(context, resolution) {
-    return this._attachments.map((attach) => {
+  createTextures(context) {
+    for (var attach of this._attachments) {
       context.activeTexture(context.TEXTURE0);
       var tex = context.createTexture();
       context.bindTexture(context.TEXTURE_2D, tex);
-      context.texStorage2D(context.TEXTURE_2D, this._levels, attach.internalFormat, resolution[0], resolution[1]);
-      //context.generateMipmap(context.TEXTURE_2D); // TODO: clear first? ...
-      //this._levels = context.getTexParameter(context.TEXTURE_2D, context.TEXTURE_MAX_LEVEL);
+      context.texStorage2D(context.TEXTURE_2D, 1, attach.internalFormat, this._resolution[0], this._resolution[1]);
       context.bindTexture(context.TEXTURE_2D, null);
+      attach.addTexture(tex);
 
-      return tex;
-    });
+      context.activeTexture(context.TEXTURE0);
+      var tex = context.createTexture();
+      context.bindTexture(context.TEXTURE_2D, tex);
+      context.texStorage2D(context.TEXTURE_2D, 1, attach.internalFormat, this._resolution[0], this._resolution[1]);
+      context.bindTexture(context.TEXTURE_2D, null);
+      attach.addTexture(tex);
+    }
+  }
+
+  createFramebuffers(context) {
+    this._framebuffers = [context.createFramebuffer(), context.createFramebuffer()];
+
+    for (var i = 0; i < this._framebuffers.length; i++) {
+      var fbo = this._framebuffers[i];
+      context.bindFramebuffer(context.DRAW_FRAMEBUFFER, fbo);
+
+      for (var j = 0; j < this._attachments.length; j++) {
+        var attach = this._attachments[j];
+        var tex = attach.textures[(i+1) % 2];
+        context.framebufferTexture2D(context.DRAW_FRAMEBUFFER, context[`COLOR_ATTACHMENT${j}`], context.TEXTURE_2D, tex, 0);
+      }
+
+      this.checkFboStatus(context);
+      context.clearColor(0.0,0.0,0.0,0.0);
+      context.clear(context.COLOR_BUFFER_BIT);
+      context.bindFramebuffer(context.DRAW_FRAMEBUFFER, null);
+    }
+  }
+
+  generateMipmaps(resolution, levels) {
+    var firstLevel = {
+      pos: vec2.fromValues(0,0),
+      res: vec2.fromValues(Math.ceil(resolution[0] / 2), Math.ceil(resolution[1] / 2))
+    };
+
+    return new Int32Array(levels - 1).reduce((mips, level, i) => {
+      var pos = vec2.create(), res;
+      if (i % 2 == 0) {
+        vec2.add(pos, mips[i].pos, vec2.fromValues(mips[i].res[0], 0));
+      } else {
+        vec2.add(pos, mips[i].pos, vec2.fromValues(0, mips[i].res[1]));
+      }
+
+      // each level is half the size
+      res = vec2.fromValues(
+        Math.ceil(mips[i].res[0] / 2),
+        Math.ceil(mips[i].res[1] / 2)); // Math.round is incorrect for non-power-of-two
+
+      mips[i+1] = { pos: pos, res: res };
+      return mips
+    }, [firstLevel]);
   }
 
   checkFboStatus(context) {
     var status = context.checkFramebufferStatus(context.DRAW_FRAMEBUFFER);
     if (status != context.FRAMEBUFFER_COMPLETE) {
       console.error('fb status: ' + status.toString(10));
-    }
-  }
-
-  createFramebuffer(context) {
-    var fbo = context.createFramebuffer();
-    context.bindFramebuffer(context.DRAW_FRAMEBUFFER, fbo);
-    for (var i=0; i < this._textures.length; i++) {
-      context.framebufferTexture2D(context.DRAW_FRAMEBUFFER, context[`COLOR_ATTACHMENT${i}`], context.TEXTURE_2D, this._textures[i], 0);
-    }
-    this.checkFboStatus(context);
-    context.clearColor(0.0,0.0,0.0,0.0);
-    context.clear(context.COLOR_BUFFER_BIT);
-    context.bindFramebuffer(context.DRAW_FRAMEBUFFER, null);
-    return fbo;
-  }
-
-  createMipmaps(context) {
-    for (var tex of this._textures) {
-      context.activeTexture(context.TEXTURE0);
-      context.bindTexture(context.TEXTURE_2D, tex);
-      context.generateMipmap(context.TEXTURE_2D);
-      context.bindTexture(context.TEXTURE_2D, null);
     }
   }
 
@@ -142,11 +165,22 @@ class MipReducer {
   generateFragmentShaderSource() {
     var symbols = this.fragmentShaderSymbols();
 
-    var uniforms = `
-    uniform int u_mipmapLevel;
+    var varyings = `
     in vec4 v_position;
     in vec2 v_st;
     `;
+
+    var uniforms = `
+    uniform int u_iteration;
+    uniform ivec2 u_readOffset;
+    uniform ivec2 u_readSize;
+    uniform ivec2 u_writeOffset;
+    uniform ivec2 u_writeSize;
+    `;
+
+    var additionalUniforms = Object.keys(this._uniforms).map((k) => {
+      return `uniform ${this._uniforms[k]} ${k};`;
+    }).join("\n");
 
     var samplerUniforms = this._attachments.map((attach, i) => {
       var attachSampler = symbols.sampler(attach, i);
@@ -165,9 +199,8 @@ class MipReducer {
     }).join("\n");
 
     var constructTexelCoords = `
-    ivec2 writeUv = ivec2(trunc(gl_FragCoord.xy));
-    ivec2 readUv = writeUv * 2;
-    int fetchMipmapLevel = (u_mipmapLevel == 0 ? 0 : u_mipmapLevel - 1);
+    ivec2 writeUv = ivec2(trunc(gl_FragCoord.xy)) - u_writeOffset;
+    ivec2 readUv = u_readOffset + writeUv * 2;
     bvec2 singleFetch;
     `;
 
@@ -179,12 +212,24 @@ class MipReducer {
       return `
       vec4 ${texels}[4];
 
-      singleFetch = equal(readUv, textureSize(${sampler}, fetchMipmapLevel));
-      ${texels}[0] = texelFetch(${sampler}, readUv, fetchMipmapLevel);
-      ${texels}[1] = singleFetch.x ? ${defaultValue} : texelFetch(${sampler}, readUv + ivec2(1,0), fetchMipmapLevel);
-      ${texels}[2] = singleFetch.y ? ${defaultValue} : texelFetch(${sampler}, readUv + ivec2(0,1), fetchMipmapLevel);
-      ${texels}[3] = any(singleFetch.xy) ? ${defaultValue} : texelFetch(${sampler}, readUv + ivec2(1,1), fetchMipmapLevel);
+      singleFetch = equal(readUv + ivec2(1,1), u_readOffset + u_readSize);
+      ${texels}[0] = texelFetch(${sampler}, readUv, 0);
+      ${texels}[1] = singleFetch.x ? ${defaultValue} : texelFetch(${sampler}, readUv + ivec2(1,0), 0);
+      ${texels}[2] = singleFetch.y ? ${defaultValue} : texelFetch(${sampler}, readUv + ivec2(0,1), 0);
+      ${texels}[3] = any(singleFetch.xy) ? ${defaultValue} : texelFetch(${sampler}, readUv + ivec2(1,1), 0);
       `;
+    }).join("\n");
+
+    var initAttachments = this._attachments.map((attach, i) => {
+      var texels = symbols.texels(attach, i);
+      var sampler = symbols.sampler(attach, i);
+      var defaultValue = symbols.default(attach, i);
+
+      if (attach.init) {
+        return eval("`" + attach.init + "`");
+      } else {
+        return "\n";
+      }
     }).join("\n");
 
     var calcAttachments = this._attachments.map((attach, i) => {
@@ -207,7 +252,9 @@ class MipReducer {
     }).join("\n");
 
     return `
+    ${varyings}
     ${uniforms}
+    ${additionalUniforms}
     ${samplerUniforms}
     ${attachmentOuts}
 
@@ -215,6 +262,9 @@ class MipReducer {
 
     ${constructTexelCoords}
     ${readAttachments}
+    if (u_iteration == -1) {
+      ${initAttachments}
+    }
     ${calcAttachments}
     ${writeAttachments}
 
@@ -298,110 +348,92 @@ class MipReducer {
     }
   }
 
-  encodeLevel(context, mipmapLevel, uniforms, ops = {}) {
-    context.uniform1i(this._uniformLocations.u_mipmapLevel, mipmapLevel);
-
-    this.encodeUniforms(context, uniforms, ops);
-    this.encodeTextures(context, uniforms, ops);
-
-    // TODO: ensure that, on the second draw, the correct textures are being bound
-
-    this.encodeDraw(context, uniforms, ops);
-  }
-
-  prepareReduce(context, uniforms, ops = {}) {
-    context.useProgram(this._program);
-    context.bindFramebuffer(context.DRAW_FRAMEBUFFER, this._framebuffer);
+  encodeLevel(context, iteration, uniforms, ops = {}) {
+    context.bindFramebuffer(context.DRAW_FRAMEBUFFER, this._framebuffers[this._pingpongId]);
     context.blendFunc(context.ONE, context.ZERO);
-    context.viewport(0,0,this._resolution[0], this._resolution[1]);
+
+    context.uniform1i(this._uniformLocations.u_iteration, iteration);
+    if (iteration == -1) {
+      context.uniform2iv(this._uniformLocations.u_readOffset, vec2.fromValues(0,0));
+      context.uniform2iv(this._uniformLocations.u_readSize, this._resolution);
+    } else {
+      context.uniform2iv(this._uniformLocations.u_readOffset, this._mipmaps[iteration].pos);
+      context.uniform2iv(this._uniformLocations.u_readSize, this._mipmaps[iteration].res);
+    }
+    context.uniform2iv(this._uniformLocations.u_writeOffset, this._mipmaps[iteration+1].pos);
+    context.uniform2iv(this._uniformLocations.u_writeSize, this._mipmaps[iteration+1].res);
+
+    context.viewport(
+      this._mipmaps[iteration+1].pos[0],
+      this._mipmaps[iteration+1].pos[1],
+      this._mipmaps[iteration+1].res[0],
+      this._mipmaps[iteration+1].res[1]);
+
   }
 
-  encodeUniforms(context, uniforms, ops = {}) {
-
-  }
-
-  encodeTextures(context, uniforms, ops = {}) {
+  encodeTextures(context, iteration, uniforms, ops = {}) {
     for (var i = 0; i < this._attachments.length; i++) {
       var attach = this._attachments[i];
+      var texture;
+      if (iteration == -1) {
+        texture = ops[attach.id]
+      } else {
+        texture = attach.textures[this._pingpongId];
+      }
       context.activeTexture(context[`TEXTURE${i}`]);
       context.uniform1i(attach.samplerUniformLocation, i);
       context.uniform4fv(attach.samplerDefaultUniformLocation, attach.defaultValue);
-      context.bindTexture(context.TEXTURE_2D, ops[attach.id]);
+      context.bindTexture(context.TEXTURE_2D, texture);
       context.bindSampler(i, this._samplerNearest);
     }
   }
 
   encodeDraw(context, uniforms, ops) {
-    context.drawBuffers(this._textures.map((tex, i) => context[`COLOR_ATTACHMENT${i}`]));
+    context.drawBuffers(this._attachments.map((attach, i) => context[`COLOR_ATTACHMENT${i}`]));
 
     context.bindVertexArray(ops.quad.vertexArray);
     context.drawArrays(context.TRIANGLES, 0, 6);
-  }
 
-  updateFramebufferMipmaps(context, mipmapLevel) {
-    for (var i=0; i < this._textures.length; i++) {
-      context.framebufferTexture2D(context.DRAW_FRAMEBUFFER, context[`COLOR_ATTACHMENT${i}`], context.TEXTURE_2D, this._textures[i], mipmapLevel);
-    }
-    this.checkFboStatus(context);
+    this.incrementPingpong();
   }
 
   reduce(context, uniforms, ops = {}) {
-    this.prepareReduce(context, uniforms, ops);
+    context.useProgram(this._program);
 
-    // reading from the initial textures
-    // - reading (level: 0) writing (level: 0)
-    this.encodeLevel(context, 0, uniforms, ops);
+    this._pingpongId = 0;
+    this.encodeLevel(context, -1, uniforms, ops);
+    this.encodeTextures(context, -1, uniforms, ops);
 
-    // TODO: binding [Texture 28] for [Attachment 0] (incorrect)
-    // - but binding [Texture 53] for [Attachment 1]
-    // - this reduce/merge is incorrect
+    this.checkFboStatus(context);
+    this.encodeDraw(context, uniforms, ops);
 
-    var attachTextures = this._attachments.reduce((acc, attach, i) => {
-      acc[attach.id] = this._textures[i];
+    for (var i = 0; i < this._levels - 1; i++) {
+      this.encodeLevel(context, i, uniforms, ops);
+      this.encodeTextures(context, i, uniforms, ops);
+
+      this.checkFboStatus(context);
+      this.encodeDraw(context, uniforms, ops);
+    }
+
+    context.bindFramebuffer(context.READ_FRAMEBUFFER, this._framebuffers[(this._pingpongId + 1) % 2]);
+
+    this._values = this._attachments.reduce((acc, attach, i) => {
+      var pixels = new Float32Array(4);
+      var mipmap = this._mipmaps[this._mipmaps.length - 1];
+      context.readBuffer(context[`COLOR_ATTACHMENT${i}`]);
+      context.readPixels(mipmap.pos[0], mipmap.pos[1], 1, 1, context.RGBA, context.FLOAT, pixels);
+      acc[attach.id] = pixels;
       return acc
     }, {});
 
-    var ops = Object.assign({}, ops, attachTextures);
-
-    context.bindFramebuffer(context.READ_FRAMEBUFFER, this._framebuffer);
-    var debugPixels = new Float32Array(16);
-    context.readBuffer(context.COLOR_ATTACHMENT0);
-
-    context.readPixels(0, 0, 2, 2, context.RGBA, context.FLOAT, debugPixels);
-    console.log(`MomentumSum 0`, debugPixels);
-    console.log(debugPixels[0], debugPixels[4], debugPixels[8], debugPixels[12]);
-    context.readBuffer(context.COLOR_ATTACHMENT1);
-    context.readPixels(0, 0, 2, 2, context.RGBA, context.FLOAT, debugPixels);
-    console.log(`MomentumMinMax 0`, debugPixels);
-    console.log(debugPixels[0], debugPixels[4], debugPixels[8], debugPixels[12]);
-
-    for (var i = 1; i < this._levels; i++) {
-      // - reading (level: 0) writing (level: 1)
-        // - reading (level: 1) writing (level: 2)
-      this.updateFramebufferMipmaps(context, i);
-      this.encodeLevel(context, i - 1, uniforms, ops);
-
-      context.bindFramebuffer(context.READ_FRAMEBUFFER, this._framebuffer);
-      var debugPixels = new Float32Array(16);
-      var readPixels = ((i == this._levels - 1) ? 1 : 2);
-
-      context.readBuffer(context.COLOR_ATTACHMENT0);
-      context.readPixels(0, 0, readPixels, readPixels, context.RGBA, context.FLOAT, debugPixels);
-      console.log(`MomentumSum ${i}`, debugPixels);
-      console.log(debugPixels[0], debugPixels[4], debugPixels[8], debugPixels[12]);
-      context.readBuffer(context.COLOR_ATTACHMENT1);
-      context.readPixels(0, 0, readPixels, readPixels, context.RGBA, context.FLOAT, debugPixels);
-      console.log(`MomentumMinMax ${i}`, debugPixels);
-    }
-
-    //context.bindFramebuffer(context.DRAW_FRAMEBUFFER, null);
-    //context.bindFramebuffer(context.READ_FRAMEBUFFER, this._framebuffer);
-    //context.readBuffer(context.COLOR_ATTACHMENT0);
-    //var debugPixels = new Float32Array(4);
-    //context.readPixels(0, 0, 1, 1, context.RGBA, context.FLOAT, debugPixels);
-    //context.readBuffer(context.COLOR_ATTACHMENT0);
+    return this._values;
+    // TODO: callback?
   }
 
 
-
+  incrementPingpong() {
+    this._pingpongId = (this._pingpongId + 1) % 2;
+  }
 }
+
+var outputTexture = false;
