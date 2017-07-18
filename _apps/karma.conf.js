f6406bed7b01421628fc6c26cefaa718fd8c6a64
David Conner <dconner.pro@gmail.com>
2017-07-18 18:19:21 -0400

    updates to karma.conf.js

--- _apps/karma.conf.js
+++ _apps/karma.conf.js
@@ -1,40 +1,32 @@
 const webpack = require('webpack');
 const path = require('path');
 
-const debug = (process.argv.slice(3)).some(argv => argv === '--debug');
-const watch = (process.argv.slice(3)).some(argv => argv === '--watch');
-
 "use strict";
 
 // TODO: add multiple entry points, so that each animation can have it's own tests
 // TODO: add a karma.all.conf.js to run all tests
-  // files: ['test/index.js'],
-  // preprocessors: {
-  //   'test/index.js': ['webpack', 'sourcemap']
-  // },
+// files: ['test/index.js'],
+// preprocessors: {
+//   'test/index.js': ['webpack', 'sourcemap']
+// },
 
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
     browsers: ['Chrome'],
-    singleRun: !debug,
-    autowatch: watch,
-
     frameworks: ['tap'],
-    browserDisconnectTimeout: 100000,
-    browserNoActivityTimeout: 100000,
+
+    browserDisconnectTimeout: 2000,
+    browserNoActivityTimeout: 10000,
 
     files: [
-      'test/utils/*.test.js'
+      './test/lib/*.test.js'
     ],
     preprocessors: {
-      'test/utils/*.test.js': ['webpack', 'sourcemap']
+      './test/lib/*.test.js': ['webpack', 'sourcemap']
     },
 
-    reporters: ['tap-pretty', 'html'],
-    tapReporter: {
-      prettifier: 'tap-spec',
-      sepparator: true
-    },
+    reporters: ['html', 'tap'],
+
     htmlReporter: {
       outputFile: './test/test-results.html',
       pageTitle: 'Tests',
@@ -42,11 +34,12 @@ module.exports = function(config) {
       useCompactStyle: true,
       useLegacyStyle: true
     },
+
     webpack: {
       devtool: 'inline-source-map',
       module: {
         loaders: [
-          { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+          {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}
           // {test: /\.json$/, loader: 'json-loader'}
         ]
       },
@@ -57,8 +50,11 @@ module.exports = function(config) {
         extensions: ['.js', '.json']
       }
     },
+
     webpackServer: {
       noInfo: true
     }
-  })
+
+    // plugins: [ ] // defaults to 'karma-*'
+  });
 };
--- _apps/test/lib/PingPongFBO.test.js
+++ _apps/test/lib/PingPongFBO.test.js
@@ -4,7 +4,6 @@ import { PingPongFBO } from '../../lib/PingPongFBO.js';
 "use strict";
 
 test('there is a class called PingPongFBO', t => {
-
   t.plan(1);
 
   var pingPongFBO = new PingPongFBO();
@@ -13,9 +12,9 @@ test('there is a class called PingPongFBO', t => {
 
 });
 
-
 // TODO: test generatePrefix()
 // TODO: test #version
 // TODO: test #extension
 // TODO: test precision
 // TODO: test #define
+
