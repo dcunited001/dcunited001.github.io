863f57774ff28e6cec004be3c549cad03bb38b03
David Conner <dconner.pro@gmail.com>
2017-08-17 13:31:25 -0400

    refactor PingPongFBO constructor

--- _apps/lib/ping_pong_fbo.js
+++ _apps/lib/ping_pong_fbo.js
@@ -4,84 +4,42 @@ class PingPongFBO {
 
   constructor(gl, options = {}) {
     this._max = options.max || 2;
+    this._width = options.width;
+    this._height = options.height;
     this._current = 0;
     this._attachments = options.attachments || [];
+    this._attachmentIndex = {};
     this._textureOptions = options.textureOptions || {};
     this._framebuffers = [];
     this._textures = {};
 
-    // TODO: refactor constructor behaviors to helpers
-
-    this._keyIndex = {};
     this._keys = this._attachments.map((a, i) => {
       if (typeof a.attachmentId != 'string') {
         throw "Attachment ID null or undefined"
       }
-      this._keyIndex[a.attachmentId] = i;
       return a.attachmentId;
     });
 
-    // init this._textures and generate attachments from values shared by textureOptions
-    for (var k of this._keys) {
-      this._textures[k] = [];
-      var index = this._keyIndex[k];
-      var textureOpts = this._textureOptions[k];
-
-      Object.assign(this._attachments[index],
-        (({ format, type, target }) => ({format, type, target}))(this._textureOptions[k]))
-    }
-
-    // generate textures from textureOptions
-    for (var k of this._keys) {
-      var textureOpts = this._textureOptions[k];
-      if (textureOpts) {
-        for (var fboIndex = 0; fboIndex < this._max; fboIndex++) {
-          var texture = twgl.createTexture(gl, textureOpts);
-          this._textures[k].push(texture);
-        }
-      } else {
-        throw `TextureOptions[${k}] is null or undefined`
-      }
-    }
-
-    // create framebuffers and configure attachments
-    for (var fboIndex = 0; fboIndex < this._max; fboIndex++) {
-      var newFbo = gl.createFramebuffer();
-      gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, newFbo);
-
-      for (const [i,k] of this._keys.entries()) {
-        var attachment = this._attachments[i];
-        var attachNumber = attachment.attach || (gl.COLOR_ATTACHMENT0 + i);
-        var attachmentObject = this.getNext(k);
-
-        if (attachmentObject instanceof WebGLTexture) {
-          gl.framebufferTexture2D(gl.DRAW_FRAMEBUFFER, attachNumber, attachment.target, attachmentObject, attachment.level);
-        } else if (attachmentObject instanceof WebGLRenderbuffer) {
-          gl.framebufferRenderbuffer(gl.DRAW_FRAMEBUFFER, attachNumber, attachment.target, attachmentObject);
-        } else {
-          throw "unknown attachment type";
-          // TODO: handle TextureArray with gl.framebufferTextureLayer()
-        }
-      }
-
-      gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, null);
-      this._framebuffers.push(newFbo);
-      this.increment();
-    }
+    this._setupAttachmentOptions();
+    this._setupTextures(gl);
+    this._setupFramebuffers(gl);
+    this._setupAttachmentIndex();
   }
 
+  get width() { return this._width; }
+  get height() { return this._height; }
   get attachments() { return this._attachments; }
   get keys() { return this._keys; }
   get current() { return this._current; }
   get max() { return this._max; }
   get framebuffers() { return this._framebuffers; }
 
-  //TODO: keep getKeys() ?
-  getKeys() {
-    return this._keys.reduce((memo, k) => {
-      memo[k] = this._keyIndex[k];
-      return memo
-    }, {});
+  getAttach(k) {
+    return this._attachmentIndex[k].attach;
+  }
+
+  getIndex(k) {
+    return this._attachmentIndex[k].index;
   }
 
   getTexture(k, index, ...args) {
@@ -136,6 +94,80 @@ class PingPongFBO {
   }
 
   // TODO: reimplement mappings for fbo(i) => [attachments]
+
+  /*
+   * initialize this._textures and generate attachments from values shared by textureOptions
+   */
+  _setupAttachmentOptions() {
+    for (var i = 0; i < this._keys.length; i++) {
+      var k = this._keys[i];
+      this._textures[k] = [];
+      Object.assign(this._attachments[i],
+        (({ format, type, target }) => ({format, type, target}))(this._textureOptions[k]));
+
+      this._attachments[i].format = this._attachments[i].format || gl.RGBA;
+      this._attachments[i].internalFormat = this._attachments[i].format || gl.RGBA;
+    }
+  }
+
+  /*
+   * generate WebGL texture objects from textureOptions
+   */
+  _setupTextures(gl) {
+    for (var k of this._keys) {
+      var textureOpts = this._textureOptions[k];
+      if (textureOpts) {
+        for (var fboIndex = 0; fboIndex < this._max; fboIndex++) {
+          var texture = twgl.createTexture(gl, textureOpts);
+          this._textures[k].push(texture);
+        }
+      } else {
+        throw `TextureOptions[${k}] is null or undefined`
+      }
+    }
+  }
+
+  /*
+   * create framebuffers from attachmentOptions and configure attachments
+   */
+  _setupFramebuffers(gl) {
+    for (var fboIndex = 0; fboIndex < this._max; fboIndex++) {
+      var newFbo = gl.createFramebuffer();
+      gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, newFbo);
+
+      for (const [i,k] of this._keys.entries()) {
+        var attachment = this._attachments[i];
+        attachment.attach = attachment.attach || (gl.COLOR_ATTACHMENT0 + i);
+        var attachmentObject = this.getNext(k);
+
+        if (attachmentObject instanceof WebGLTexture) {
+          gl.framebufferTexture2D(gl.DRAW_FRAMEBUFFER, attachment.attach, attachment.target, attachmentObject, attachment.level);
+        } else if (attachmentObject instanceof WebGLRenderbuffer) {
+          gl.framebufferRenderbuffer(gl.DRAW_FRAMEBUFFER, attachment.attach, attachment.target, attachmentObject);
+        } else {
+          throw "unknown attachment type";
+          // TODO: handle TextureArray with gl.framebufferTextureLayer()
+        }
+      }
+
+      gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, null);
+      this._framebuffers.push(newFbo);
+      this.increment();
+    }
+  }
+
+  /*
+   * setup an index of attachment id's and fbo attachment id's
+   */
+  _setupAttachmentIndex() {
+    for (const [i,k] of this._keys.entries()) {
+      this._attachmentIndex[k] = {
+        index: i,
+        attach: this._attachments[i].attach
+      };
+    }
+  }
+
 }
 
 export { PingPongFBO };
