3508fe22cb724792653fd6db835ef9c301c35b5e
David Conner <dconner.pro@gmail.com>
2017-08-17 11:32:41 -0400

    update PingPongFBO and spec

--- _apps/lib/ping_pong_fbo.js
+++ _apps/lib/ping_pong_fbo.js
@@ -1,23 +1,151 @@
-// TODO: assume that data passed for textures is always a Texture monoid
-// - create using createFramebufferInfo()
-// - pass in AttachmentOptions for each texture definition
-//   - src: twgl.TextureFunc((gl, textureoptions) => {})
-
-// TODO: how to test classes like this that rely heavily on WebGL
+const twgl = require('twgl.js');
 
 class PingPongFBO {
 
-  constructor(options = {}) {
-    // this._attachmentOptions = [];
-    this._current = 0;
+  constructor(gl, options = {}) {
     this._max = options.max || 2;
+    this._current = 0;
+    this._attachments = options.attachments || [];
+    this._textureOptions = options.textureOptions || {};
     this._framebuffers = [];
+    this._textures = {};
+
+    // TODO: refactor constructor behaviors to helpers
+
+    this._keyIndex = {};
+    this._keys = this._attachments.map((a, i) => {
+      if (typeof a.attachmentId != 'string') {
+        throw "Attachment ID null or undefined"
+      }
+      this._keyIndex[a.attachmentId] = i;
+      return a.attachmentId;
+    });
+
+    // init this._textures and generate attachments from values shared by textureOptions
+    for (var k of this._keys) {
+      this._textures[k] = [];
+      var index = this._keyIndex[k];
+      var textureOpts = this._textureOptions[k];
+
+      Object.assign(this._attachments[index],
+        (({ format, type, target }) => ({format, type, target}))(this._textureOptions[k]))
+    }
+
+    // generate textures from textureOptions
+    for (var k of this._keys) {
+      var textureOpts = this._textureOptions[k];
+      if (textureOpts) {
+        if (textureOpts instanceof Array) {
+          if (textureOpts.length == this._max && textureOpts[0] instanceof WebGLTexture) {
+            this._textures[k] = textureOpts;
+          } else {
+            throw `TextureOptions[${k}] does not contain array of type WebGLTexture`;
+          }
+        } else {
+          for (var fboIndex = 0; fboIndex < this._max; fboIndex++) {
+            var texture = twgl.createTexture(gl, textureOpts);
+            this._textures[k].push(texture);
+          }
+        }
+      } else {
+        throw `TextureOptions[${k}] is null or undefined`
+      }
+    }
+
+    // create framebuffers and configure attachments
+    for (var fboIndex = 0; fboIndex < this._max; fboIndex++) {
+      var newFbo = gl.createFramebuffer();
+      gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, newFbo);
+
+      for (const [i,k] of this._keys.entries()) {
+        var attachment = this._attachments[i];
+        var attachNumber = attachment.attach || (gl.COLOR_ATTACHMENT0 + i);
+        var attachmentObject = this.getNext(k);
+
+        if (attachmentObject instanceof WebGLTexture) {
+
+console.log(gl.TEXTURE_2D, attachment.target);
+          gl.framebufferTexture2D(gl.DRAW_FRAMEBUFFER, attachNumber, attachment.target, attachmentObject, attachment.level);
+        } else if (attachmentObject instanceof WebGLRenderbuffer) {
+          gl.framebufferRenderbuffer(gl.DRAW_FRAMEBUFFER, attachNumber, attachment.target, attachmentObject);
+        } else {
+          throw "unknown attachment type";
+          // TODO: handle TextureArray with gl.framebufferTextureLayer()
+        }
+      }
+
+      gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, null);
+      this._framebuffers.push(newFbo);
+      this.increment();
+    }
   }
 
+  get attachments() { return this._attachments; }
+  get keys() { return this._keys; }
+  get current() { return this._current; }
+  get max() { return this._max; }
+  get framebuffers() { return this._framebuffers; }
 
-  //TODO: reimplemented getPrev(), getCurrent(), getNext()
-  //TODO: reimplement mappings for fbo(i) => [attachments]
+  //TODO: keep getKeys() ?
+  getKeys() {
+    return this._keys.reduce((memo, k) => {
+      memo[k] = this._keyIndex[k];
+      return memo
+    }, {});
+  }
 
+  getTexture(k, index, ...args) {
+    return this._textures[k][index];
+  }
+
+  getCurrent(k, ...args) {
+    var index = this.getCurrentId();
+    return this.getTexture(k, index, ...args);
+  }
+
+  getNext(k,  ...args) {
+    var index = this.getNextId();
+    return this.getTexture(k, index, ...args);
+  }
+
+  getPrev(k, ...args) {
+    var index = this.getPrevId();
+    return this.getTexture(k, index, ...args);
+  }
+
+  getCurrentFbo() {
+    return this._framebuffers[this.getCurrentId()];
+  }
+
+  getCurrentId() {
+    return this._current;
+  }
+
+  getNextId() {
+    if (this._current === this._max - 1) {
+      return 0;
+    } else {
+      return this._current + 1;
+    }
+  }
+
+  getPrevId() {
+    if (this._current === 0) {
+      return this._max - 1;
+    } else {
+      return this._current - 1;
+    }
+  }
+
+  increment() {
+    if (this._current === this._max - 1) {
+      this._current = 0;
+    } else {
+      this._current++;
+    }
+  }
+
+  // TODO: reimplement mappings for fbo(i) => [attachments]
 }
 
 export { PingPongFBO };
--- _apps/spec/lib/ping_pong_fbo.spec.js
+++ _apps/spec/lib/ping_pong_fbo.spec.js
@@ -1,14 +1,135 @@
 import { PingPongFBO } from '../../lib/ping_pong_fbo.js';
+const twgl = require('twgl.js');
+
+const fboPongerMax = 3;
+const canvas = document.createElement('canvas');
+const gl = canvas.getContext('webgl2');
 
 describe('PingPongFBO', () => {
-  it('is a class called PingPongFBO', () => {
-    var pingPongFBO = new PingPongFBO();
-    expect(pingPongFBO.constructor.name).to.equal('PingPongFBO');
-  });
-});
+  var fboPonger;
+  var smallTextures, smallTextureOptions, smallTextureFunc, smallTextureFuncColor;
 
-// TODO: test generatePrefix()
-// TODO: test #version
-// TODO: test #extension
-// TODO: test precision
-// TODO: test #define
+  before(() => {
+
+    smallTextureOptions = {
+      target: gl.TEXTURE_2D,
+      width: 32, height: 32,
+      format: gl.RGBA,
+      internalFormat: gl.RGBA32F,
+      type: gl.FLOAT
+    };
+
+    // sets the texture to black
+    smallTextureFunc = (gl, opts) => {
+      return new Float32Array(opts.width * opts.height * 4);
+    };
+
+    // sets the texture to a solid color, with args given at eval time
+    smallTextureFuncColor = (gl, opts) => {
+      var data = new Float32Array(opts.width * opts.height * 4);
+      data = data.map((a, i) => {
+        return (opts.srcArgs || {}).color[i % 4] || 0;
+      });
+
+      return data;
+    };
+
+    smallTextures = new Array(fboPongerMax).fill(0).map((_,i) => {
+      return twgl.createTexture(gl, Object.assign({}, smallTextureOptions, { src: smallTextureFunc }));
+    });
+
+  });
+
+  beforeEach(() => {
+    fboPonger = new PingPongFBO(gl, {
+      max: 3,
+      textureOptions: {
+        smallTexture: Object.assign({}, smallTextureOptions, {
+          src: smallTextureFunc
+        }),
+        smallTextureMonad: Object.assign({}, smallTextureOptions, {
+          src: smallTextureFuncColor,
+          srcArgs: {
+            color: [1.0, 0.0, 0.0, 1.0]
+          }
+        }),
+        smallTexturePregen: smallTextures
+      },
+      attachments: [{
+        attachmentId: 'smallTexture',
+        level: 0
+      },
+        {
+        attachmentId: 'smallTextureMonad',
+          level: 0
+        // level: 1
+      }, {
+        attachmentId: 'smallTexturePregen',
+          level: 0
+        // level: 2
+      }]
+    });
+  });
+
+  it("creates 'max' number of textures per fbo attachment and cycles through framebuffers", () => {
+    expect(fboPonger.getCurrentId()).to.equal(0);
+    expect(fboPonger.getPrevId()).to.equal(fboPonger.max - 1);
+    expect(fboPonger.getNextId()).to.equal(1);
+
+    for (var i = 0; i < fboPonger.max - 1; i++) {
+      fboPonger.increment();
+    }
+
+    expect(fboPonger.getCurrentId()).to.equal(fboPonger.max - 1);
+    expect(fboPonger.getPrevId()).to.equal(fboPonger.max - 2);
+    expect(fboPonger.getNextId()).to.equal(0);
+  });
+
+  it("can retrieve the proper textures and fbo's", () => {
+    var fboAttrs = new Array(3).fill({
+      smallTexture: {},
+      smallTextureMonad: {},
+      smallTexturePregen: {}
+    });
+
+    for (var i = 0; i < fboPongerMax; i++) {
+      var fbo = fboPonger.getCurrentFbo();
+
+      gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, fbo);
+      fboAttrs[i].smallTexture.type = gl.getFramebufferAttachmentParameter(gl.DRAW_FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.FRAMEBUFFER_ATTACHMENT_OBJECT_TYPE);
+      fboAttrs[i].smallTexture.name = gl.getFramebufferAttachmentParameter(gl.DRAW_FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.FRAMEBUFFER_ATTACHMENT_OBJECT_NAME);
+      fboAttrs[i].smallTexture.level = gl.getFramebufferAttachmentParameter(gl.DRAW_FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.FRAMEBUFFER_ATTACHMENT_TEXTURE_LEVEL);
+      // fboAttrs[i].smallTextureMonad.type = gl.getFramebufferAttachmentParameter(gl.DRAW_FRAMEBUFFER, gl.COLOR_ATTACHMENT1, gl.FRAMEBUFFER_ATTACHMENT_OBJECT_TYPE);
+      // fboAttrs[i].smallTextureMonad.name = gl.getFramebufferAttachmentParameter(gl.DRAW_FRAMEBUFFER, gl.COLOR_ATTACHMENT1, gl.FRAMEBUFFER_ATTACHMENT_OBJECT_NAME);
+      // fboAttrs[i].smallTextureMonad.level = gl.getFramebufferAttachmentParameter(gl.DRAW_FRAMEBUFFER, gl.COLOR_ATTACHMENT1, gl.FRAMEBUFFER_ATTACHMENT_OBJECT_LEVEL);
+      // fboAttrs[i].smallTexturePregen.type = gl.getFramebufferAttachmentParameter(gl.DRAW_FRAMEBUFFER, gl.COLOR_ATTACHMENT2, gl.FRAMEBUFFER_ATTACHMENT_OBJECT_TYPE);
+      // fboAttrs[i].smallTexturePregen.name = gl.getFramebufferAttachmentParameter(gl.DRAW_FRAMEBUFFER, gl.COLOR_ATTACHMENT2, gl.FRAMEBUFFER_ATTACHMENT_OBJECT_NAME);
+      // fboAttrs[i].smallTexturePregen.level = gl.getFramebufferAttachmentParameter(gl.DRAW_FRAMEBUFFER, gl.COLOR_ATTACHMENT2, gl.FRAMEBUFFER_ATTACHMENT_OBJECT_LEVEL);
+      // expect(gl.checkFramebufferStatus(gl.DRAW_FRAMEBUFFER)).to.equal(gl.FRAMEBUFFER_COMPLETE);
+      console.log(gl.checkFramebufferStatus(gl.DRAW_FRAMEBUFFER));
+      gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, null);
+
+      var fooPixels = new Float32Array(32 * 32 * 4).fill(16);
+      gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
+      console.log(gl.checkFramebufferStatus(gl.FRAMEBUFFER));
+      gl.readPixels(0,0,32,32,gl.RGBA, gl.FLOAT, fooPixels);
+
+
+      console.log(fooPixels[0]);
+      console.log(fboAttrs[i]);
+      fboPonger.increment();
+    }
+
+    console.log(fboAttrs);
+
+  });
+
+  // it("can fetch data from an fbo attachment using readPixels()", () => {
+  //
+  // })
+  //
+  // it("can generate, update, regenerate a texture via a monoid or a set of sourcetypes", () => {
+  //
+  // });
+
+});
