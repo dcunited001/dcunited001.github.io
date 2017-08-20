7bd080c6e1688de45aea556a4f80ba612417602b
David Conner <dconner.pro@gmail.com>
2017-08-20 01:00:08 -0400

    * add basic implementation of dat.gui & Animation.Input
    * implement Util.Platform helpers

--- _apps/2017-07-10-the-topography-of-a-field/es6/main.js
+++ _apps/2017-07-10-the-topography-of-a-field/es6/main.js
@@ -5,7 +5,9 @@ import { GUI } from 'dat.gui/build/dat.gui';
 import { MipReducerAttachment, MipReducer } from '../../lib/MipReducer.js';
 
 import { PingPongFBO } from "../../lib/ping_pong_fbo"
-import { Animation, Input as AniInput, Timer, Platform as AniPlatform } from '../../lib/animation.js';
+import { Animation, Input as AniInput, Timer } from '../../lib/animation';
+
+import { Platform as AniPlatform } from '../../lib/util';
 
 const twgl = require('twgl.js');
 
@@ -25,38 +27,32 @@ const twgl = require('twgl.js');
 var UINT32_MAX = (2 ** 32) - 1;
 var INT32_MAX = (2 ** 31) - 1;
 
-function platformWarnings(gl) {
-  var container = document.getElementById('animation-alerts');
-
-  if (!AniPlatform.checkEs6()) { AniPlatform.appendLabel(container, "Requires ES6"); }
-  if (AniPlatform.checkMobile()) { AniPlatform.appendLabel(container, "Requires Desktop Browser"); }
-  if (!!gl) { AniPlatform.appendLabel(container, "Requires WebGL2"); }
-}
-
-platformWarnings(gl);
-console.log
+var canvas = document.getElementById('main-canvas');
+var gl = canvas.getContext('webgl2', { antialias: true });
 
 var gui = new GUI();
-
-var canvas = document.getElementById('main-canvas');
-var gl = canvas.getContext('webgl2', {antialias: true});
 
 function setupDatGUI(gui, data, container) {
   container.appendChild(gui.domElement);
 
-  gui.add(data.particleCount, 'Particle Count', 32, 2048);
+  gui.add(data, 'particleCount', 32, 2048);
 
-  var rFieldGui = gui.addFolder('R-Field');
-  rFieldGui.add(data.rField.coefficient, 'coefficient').min(0.001).max(10).step(0.0001);
-  rFieldGui.add(data.rField.size, 'size').min(1).max(300).step(1);
-
+  var rFieldGui = gui.addFolder('Repulsion Field');
+  rFieldGui.add(data.rField, 'coefficient').min(0.001).max(10).step(0.0001);
+  rFieldGui.add(data.rField, 'size').min(1).max(300).step(1);
 }
 
 function runWebGL() {
-  var canvas = document.getElementById('main-canvas');
 
   // TODO: developer implements rStats
   // var rstats =
+
+  var vaoAttributes = ['a_position', 'a_texcoord'];
+
+  // TODO: read in vs/fs programs
+  // TODO: create render passes
+  // TODO: create textures/resources
+  // TODO: create geometry
 
   // setup UI Controller objects
   // - plain js objects with a few functions
@@ -66,51 +62,87 @@ function runWebGL() {
   // in animation, have an uiStateUpdate() method or callback
   // - so
 
-  var animationControls = {
-    btnPlay: new AnimationControl(document.getElementById('btn-play')),
-    btnMic: new AnimationControl(document.getElementById('btn-mic')),
-    btnParticleReset: new AnimationControl(document.getElementById('btn-particle-reset'))
+  // var animationControls = {
+  //   btnPlay: new AnimationControl(document.getElementById('btn-play')),
+  //   btnMic: new AnimationControl(document.getElementById('btn-mic')),
+  //   btnParticleReset: new AnimationControl(document.getElementById('btn-particle-reset'))
+  // };
+  //
+  // var animationInputs = {
+  //   particleCount: new AnimationInput(document.getElementById('particle-count')),
+  //   simulationRate: new AnimationInput(document.getElementById('simulation-rate')),
+  //   rCoefficient: new AnimationInput(document.getElementById('r-coefficient')),
+  //   fieldSize: new AnimationInput(document.getElementById('field-size')),
+  //   clampField: new AnimationInput(documents.getElementById('clamp-field'))
+  // };
+
+  var guiInputs = {
+    particleCount: 256,
+    rField: {
+      coefficient: 1,
+      size: 64
+    }
   };
 
-  var animationInputs = {
-    particleCount: new AnimationInput(document.getElementById('particle-count')),
-    simulationRate: new AnimationInput(document.getElementById('simulation-rate')),
-    rCoefficient: new AnimationInput(document.getElementById('r-coefficient')),
-    fieldSize: new AnimationInput(document.getElementById('field-size')),
-    clampField: new AnimationInput(documents.getElementById('clamp-field'))
+  setupDatGUI(gui, guiInputs, document.getElementById('dat-gui-container'));
+
+  var aniInputs = {
+    physicsMethod: new AniInput(document.getElementsByName('physics-method'), {
+      fetch: function () {
+        var uiRadios = this._source;
+        return [0,1,2].reduce((a,i) => uiRadios [i].checked ? i : a, 0);
+      },
+      update: function (newValue) {
+        // update radio buttons
+      }
+    })
   };
 
-  var animationUI = new AnimationUI(canvas, animationInputs, animationControls, {});
-  var animation = Animation(animationUI);
+  //================================================
+  // TODO: keep dat.gui & aniInputs separate, collect & join across
+  //================================================
+
+  var animationState = {
+
+  };
+
+  var animationUpdateState = function() {
+    // update animation
+  };
+
+  var animationUpdate = function() {
+    // update physics
+  };
+
+  var animationDraw = function() {
+    // set uniforms
+    // render
+  };
+
+  var animation = new Animation({
+    state: animationState,
+    updateState: animationUpdateState,
+    update: animationUpdate,
+    animationDraw: animationDraw
+  });
 
   // TODO: set up render/draw method
   // TODO: wire up UI events
   // TODO: create shader prefix
-
-  var vaoAttributes = ['a_position', 'a_texcoord'];
-
-  // TODO: read in vs/fs programs
-
-
-
-
-  // TODO: create textures/resources
-
-  // TODO: create geometry
-
-  // run
-  
 }
 
 
+function platformWarnings(gl) {
+  var container = document.getElementById('animation-alerts');
 
-// TODO: render pass
-class RenderPass {
-  // render pass is mostly handled by twgl
-  // - already encodes uniforms
-  // - already encodes textures with samplers
-
-  // TODO:
-  //   - encode gl.drawBuffers();
-  //   - set blending & stenciling options
+  if (!AniPlatform.checkEs6()) { AniPlatform.appendLabel(container, "Requires ES6"); }
+  if (AniPlatform.checkMobile()) { AniPlatform.appendLabel(container, "Requires Desktop Browser"); }
+  if (!!gl) { AniPlatform.appendLabel(container, "Requires WebGL2"); }
 }
+
+window.onload = function() {
+  platformWarnings(gl);
+  AniPlatform.fixCanvasUIBar(...['main-canvas-container', 'main-canvas', 'canvas-ui-bar-bottom'].map(id => document.getElementById(id)));
+
+  runWebGL();
+};
